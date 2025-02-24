fa462896b748751ddb22fefd8f1cfd52a4874285
Alexis 18 <dath_18@outlook.com>
2025-02-24 00:52:05 -0600

    Programa Principal sin Cambios

---
+++ src/main/java/mx/uv/fiee/iinf/poo/calculator/Main.java
@@ -0,0 +1,19 @@
+package mx.uv.fiee.iinf.poo.calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Integer[] intNums = {1, 2, 3, 4, 5};
+        Calculator<Integer> intCalc = new Calculator<Integer>(intNums);
+        System.out.println(intCalc.average());
+
+        Double[] doubleNums = {1.1, 2.2, 3.3, 4.4, 5.5};
+        Calculator<Double> doubleCalc = new Calculator<Double>(doubleNums);
+        System.out.println(doubleCalc.average());
+
+        // Pero deberá fallar si se intenta ejecutar lo siguiente
+//        String [] strNums = { "1", "2", "3", "4", "5" };
+//        Calculator<String> strCalc = new Calculator<String> (strNums);
+//        System.out.println (strCalc.average ());
+    }
+}
